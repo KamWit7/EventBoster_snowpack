94fc391f2a521db1b389ed73a59ac4b7726f26b2
Przemysław Kamiński <kamykprzemek@gmail.com>
2021-10-23 20:17:58 +0200

    modal-show

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,24 @@
 console.log("modal window")
+const modalShow = document.querySelectorAll('.event');
+const modal = document.querySelector('.wrapper')
+const backdrop = document.querySelector('.backdrop')
+const body = document.querySelector('body')
+console.log(modalShow)
+
+function openModal(){
+    modal.classList.add('wrapper-active');
+    backdrop.classList.remove('is-hidden')
+    body.classList.add('over')
+}
+
+function closeModal(){
+    modal.classList.remove('wrapper-active');
+    backdrop.classList.add('is-hidden')
+    body.classList.remove('over')
+}
+
+modalShow.forEach(event =>{
+    event.addEventListener('click', openModal)
+})
+
+backdrop.addEventListener('click', closeModal)
